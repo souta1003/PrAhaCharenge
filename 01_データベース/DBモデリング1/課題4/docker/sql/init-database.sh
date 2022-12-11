b4fd35ed295aa32a69cc6488d26235edd62f09dc
sou <sta.100100@gmail.com>
2022-12-11 22:37:26 +0900

    キャッシュ削除

--- 01_データベース/DBモデリング1/課題4/docker/sql/init-database.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-#run the setup script to create the DB and the schema in the DB
-mysql --defaults-extra-file=/etc/mysql/conf.d/my.cnf -t --show-warnings sushi_test < "/docker-entrypoint-initdb.d/customer.ddl"
-mysql --defaults-extra-file=/etc/mysql/conf.d/my.cnf -t --show-warnings sushi_test < "/docker-entrypoint-initdb.d/customer_detail.ddl"
-mysql --defaults-extra-file=/etc/mysql/conf.d/my.cnf -t --show-warnings sushi_test < "/docker-entrypoint-initdb.d/product.ddl"
